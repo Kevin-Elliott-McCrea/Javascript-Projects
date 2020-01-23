48954400ba43bbd1779b4bbc3df77014aaa45b1e
Kevin Elliott McCrea <kleiben@gmail.com>
2020-01-23 10:45:50 -0800

    First push

---
+++ Basic_Javascript_Projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,12 @@
+var soccer = "ball", romance = "flowers", happy = "yeehaw"; 
+    
+    var soccer = soccer.fontcolor("orange");
+    var romance = romance.fontcolor("blue");
+    var happy = happy.fontcolor("green");
+
+T = 3+3
+
+
+
+
+document.write(happy)
